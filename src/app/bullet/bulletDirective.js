fdb6521298f2437b05a4a3e49350fabacdd86b6c
l-lin <louis.lin.87@gmail.com>
2013-12-20 21:30:28 +0100

    Correct keyboard events

--- src/app/bullet/bulletDirective.js
+++ src/app/bullet/bulletDirective.js
@@ -2,15 +2,12 @@
 	ng.module('bullet').directive('focusMe', function($timeout, $parse) {
         return {
             restrict: 'A',
-            link: function(scope, element, attrs) {
-                var model = $parse(attrs.focusMe);
-                scope.$watch(model, function(value) {
-                    if (value) {
-                        $timeout(function() {
-                            var $element = ng.element(element[0]);
-                            element[0].focus();
-                            $element.val($element.val());
-                        });
+            scope: {trigger: '=focusMe'},
+            link: function($scope, $element, attrs) {
+                $scope.$watch('trigger', function(value) {
+                    if(value === true) { 
+                        $element[0].focus();
+                        $scope.trigger = false;
                     }
                 });
             }
@@ -24,13 +21,12 @@
             scope: {
                 items: '='
             },
-            link: function($scope, $element, attrs) {
+            link: function($scope, element, attrs) {
+                var currentIndex = 0;
+                
                 $scope.addItem = function addItem($event, index) {
-                    $scope.items.splice(index + 1, 0, {
-                        text: '',
-                        focus: true,
-                        complete: false
-                    });
+                    $scope.items.splice(index + 1, 0, bulletFactory.newItem(''));
+                    $scope.selectItem(index + 1);
                     $event.preventDefault();
                 };
 
@@ -40,12 +36,39 @@
                 };
                 
                 $scope.removeItem = function removeItem($event, index) {
+                    var indexToFocus = 0;
+                    
                     $scope.items.splice(index, 1);
                     if($scope.items.length === 0) {
-                        $scope.items.push(bulletFactory.newItem('', true, true));
+                        $scope.items.push(bulletFactory.newItem(''));
+                        $scope.selectItem(0);
                     }
+                    
+                    if($scope.items[index]) {
+                        indexToFocus = index;
+                    } else if ($scope.items[index - 1]) {
+                        indexToFocus = index - 1;
+                    }
+                    $scope.selectItem(indexToFocus);
+                    
                     $event.preventDefault();
                 };
+                
+                $scope.selectItem = function selectItem(index) {
+                    if(index >= 0 && index < $scope.items.length) {
+                        if($scope.items[currentIndex]) {
+                            $scope.items[currentIndex].focus = false;
+                        }
+                        currentIndex = index;
+                        $scope.items[index].focus = true;
+                    }
+                };
+                
+                $scope.addSubItem = function addSubItem($event, index) {
+                    console.log('foo');
+                    $scope.items[index].subItems.push(bulletFactory.newItem(''));
+                    $event.preventDefault();
+                }
             }
         }
     });
--- src/app/bullet/bulletFactory.js
+++ src/app/bullet/bulletFactory.js
@@ -1,11 +1,12 @@
 (function(ng) {
 	ng.module('bullet').factory('bulletFactory', function() {
 		return {
-            newItem: function(text, focus, complete) {
+            newItem: function(text, focus, complete, subItems) {
                 return {
                     text: text,
                     focus: focus || false,
-                    complete: complete || false
+                    complete: complete || false,
+                    subItems: subItems || []
                 };
             }
         };

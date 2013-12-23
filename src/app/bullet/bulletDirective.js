c7cc9e41c93d415896094b1513bcdf7519ee82aa
unknown <l.lin@LLNDEV.oodrive.net>
2013-12-23 17:34:39 +0100

    Add sub tasks

--- src/app/bullet/bulletDirective.js
+++ src/app/bullet/bulletDirective.js
@@ -6,72 +6,96 @@
             scope: {
                 trigger: '=focusMe'
             },
-            link: function($scope, $element) {
-                $scope.$watch('trigger', function(value) {
+            link: function(scope, element) {
+                scope.$watch('trigger', function(value) {
                     if (value === true) {
-                        $element[0].focus();
-                        $scope.trigger = false;
+                        element[0].focus();
+                        scope.trigger = false;
                     }
                 });
             }
         };
     });
 
-    angular.module('bullet').directive('bullets', function(bulletFactory) {
+    angular.module('bullet').directive('bullets', function(bulletFactory, $compile) {
         return {
             restrict: 'E',
             templateUrl: 'app/bullet/bullets.html',
             scope: {
                 items: '='
             },
-            link: function($scope) {
+            link: function(scope, element) {
                 var currentIndex = 0;
 
-                $scope.addItem = function addItem($event, index) {
-                    $scope.items.splice(index + 1, 0, bulletFactory.newItem(''));
-                    $scope.selectItem(index + 1);
+                scope.addItem = function addItem($event, index) {
+                    scope.items.splice(index + 1, 0, bulletFactory.newItem(''));
+                    scope.selectItem(index + 1);
                     $event.preventDefault();
                 };
 
-                $scope.completeItem = function completeItem($event, index) {
-                    $scope.items[index].complete = !$scope.items[index].complete;
+                scope.completeItem = function completeItem($event, index) {
+                    scope.items[index].complete = !scope.items[index].complete;
                     $event.preventDefault();
                 };
 
-                $scope.removeItem = function removeItem($event, index) {
+                scope.removeItem = function removeItem($event, index) {
                     var indexToFocus = 0;
 
-                    $scope.items.splice(index, 1);
-                    if ($scope.items.length === 0) {
-                        $scope.items.push(bulletFactory.newItem(''));
-                        $scope.selectItem(0);
+                    scope.items.splice(index, 1);
+                    if (scope.items.length === 0) {
+                        scope.items.push(bulletFactory.newItem(''));
+                        scope.selectItem(0);
                     }
 
-                    if ($scope.items[index]) {
+                    if (scope.items[index]) {
                         indexToFocus = index;
-                    } else if ($scope.items[index - 1]) {
+                    } else if (scope.items[index - 1]) {
                         indexToFocus = index - 1;
                     }
-                    $scope.selectItem(indexToFocus);
+                    scope.selectItem(indexToFocus);
 
                     $event.preventDefault();
                 };
 
-                $scope.selectItem = function selectItem(index) {
-                    if (index >= 0 && index < $scope.items.length) {
-                        if ($scope.items[currentIndex]) {
-                            $scope.items[currentIndex].focus = false;
+                scope.selectItem = function selectItem(index) {
+                    if (index >= 0 && index < scope.items.length) {
+                        if (scope.items[currentIndex]) {
+                            scope.items[currentIndex].focus = false;
                         }
                         currentIndex = index;
-                        $scope.items[index].focus = true;
+                        scope.items[index].focus = true;
                     }
                 };
 
-                //                $scope.addSubItem = function addSubItem($event, index) {
-                //                    /*console.log('foo');
-                //                    $scope.items[index].subItems.push(bulletFactory.newItem(''));
-                //                    $event.preventDefault();*/
-                //                }
+                scope.toSubItem = function toSubItem($event, index) {
+                    if(scope.items[index - 1]) {
+                        scope.items[index - 1].subItems.push(scope.items[index]);
+                        scope.items.splice(index, 1);
+                    }
+                    $event.preventDefault();
+                }
+                
+                scope.toItem = function toItem($event, index) {
+                    scope.items.splice(index + 1, 0, scope.items[index]);
+                    scope.selectItem(index + 1);
+                    $event.preventDefault();
+                }
+            }
+        };
+    });
+    
+    angular.module('bullet').directive('bullet', function($compile) {
+        return {
+            restrict: 'E',
+            templateUrl: 'app/bullet/bullet.html',
+            scope: {
+                item: '='
+            },
+            link: function(scope, element) {
+                if (angular.isArray(scope.item.subItems)) {
+                    element.append('<bullets items="item.subItems"></bullets>'); 
+                    $compile(element.contents())(scope)
+                }
             }
         };
     });
--- src/app/bulletularCtrl.js
+++ src/app/bulletularCtrl.js
@@ -2,7 +2,7 @@
     'use strict';
     angular.module('bulletularApp').controller('bulletularCtrl', function($scope, bulletFactory) {
         $scope.items = [
-            bulletFactory.newItem('Developing with AngularJS', true, true),
+            bulletFactory.newItem('Developing with AngularJS', true, true, [bulletFactory.newItem('foo'), bulletFactory.newItem('foo2')]),
             bulletFactory.newItem('Add a shortcut to clean all complete tasks', false, false),
             bulletFactory.newItem('Add sub tasks', false, false),
             bulletFactory.newItem('Add localestorage for saving tasks', false, false),

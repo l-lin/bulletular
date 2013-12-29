f0584c9c5e1ff2c45e43c6c3df3dbe3157ad988c
l-lin <louis.lin.87@gmail.com>
2013-12-30 00:50:58 +0100

    Add shortcut help

--- src/app/bulletularCtrl.js
+++ src/app/bulletularCtrl.js
@@ -2,9 +2,9 @@
     'use strict';
     angular.module('bulletularApp').controller('bulletularCtrl', function($scope, bulletFactory) {
         $scope.bullets = [
-            bulletFactory.newBullet([0], 'Developing with AngularJS', true, true, [bulletFactory.newBullet([0, 0], 'foo'), bulletFactory.newBullet([0, 1], 'foo2')]),
-            bulletFactory.newBullet([1], 'Add a shortcut to clean all complete tasks', false, false),
-            bulletFactory.newBullet([2], 'Add sub tasks', false, false),
+            bulletFactory.newBullet([0], 'Developing with AngularJS', true, true),
+            bulletFactory.newBullet([1], 'Add sub tasks', false, true, [bulletFactory.newBullet([1, 0], 'Increase indent', false, true), bulletFactory.newBullet([1, 1], 'Decrease indent', false, true)]),
+            bulletFactory.newBullet([2], 'Add a shortcut to clean all complete tasks', false, false),
             bulletFactory.newBullet([3], 'Add localestorage for saving tasks', false, false),
             bulletFactory.newBullet([4], 'Add the possibility to navigate through tasks', false, false),
             bulletFactory.newBullet([5], 'Add breadcrumb', false, false)
